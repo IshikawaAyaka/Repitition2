3f67193b6553e8e80f9e016961c872f3ef0adcb3
Ishikawa Ayaka <a-ishikawa@cresco.co.jp>
2020-06-09 19:33:48 +0900

    Turn,Game変更

--- 04_実装／単体テスト/Game.java
+++ 04_実装／単体テスト/Game.java
@@ -1,3 +1,5 @@
+package guessing_game;
+
 public class Game {
 
 	private Player player;
@@ -6,8 +8,46 @@ public class Game {
 
 	private Turn turn;
 
-	public Game() {
-
+	public void start() {
+		//カードスタックの生成
+		CardStack stack = new CardStack();
+		
+		//プレイヤーの生成
+		player = new Player("A");
+		
+		//審判の選択
+		while(true) {
+			int refNum = player.selectReferee();
+		if(refNum == 1) {
+			referee = new StrictReferee(stack);
+			break;
+		}if(refNum == 2) {
+			referee = new GentleReferee(stack);
+			break;
+		}else {
+			System.out.println("1か2の数字を入力してください");
+		}
+		}
+		
+		//答えのカードの設定
+		referee.setAnswerCard();
+		
+		boolean judgement = false;
+		boolean canSelect = false;
+		
+		while(judgement == false) {
+		//ターンの生成
+		turn = new Turn(player);
+		
+			//数字、スートの入力
+		while(canSelect == false) {
+		canSelect = turn.requestCard();
+		}
+		//判定
+		Card card = turn.getCard();
+		judgement = referee.judge(card);
+		}
+		
 	}
 
 }
